14191176ed3ed118afc93415495e96d7a41d3db9
Hannan0107 <abdulhannan0107@gmail.com>
2025-05-18 03:49:29 +0530

    Initial commit for QRUG project

---
+++ padel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv
