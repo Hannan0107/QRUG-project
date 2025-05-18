551034ae76ff2ec792809efafc5176bcb77afc1d
Hannan <abdulhannan0107@gmail.com>
2025-05-18 14:01:51 +0530

    Update padel.sh 4.0

--- padel.sh
+++ padel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use JAVA_HOME set by set_java_home.sh
+# Use JAVA_HOME from the sourced script
 $JAVA_HOME/bin/java -Xms128m -Xmx256m -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar "$@"
