5b1563bbb342f5ce3f7efb31dfa6e5d514a644f4
Hannan <abdulhannan0107@gmail.com>
2025-05-18 13:58:23 +0530

    Update padel.sh 3.0

--- padel.sh
+++ padel.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Set JAVA_HOME
-JAVA_HOME=/opt/java/openjdk
-# Run Java with the correct path
+# Use JAVA_HOME set by set_java_home.sh
 $JAVA_HOME/bin/java -Xms128m -Xmx256m -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar "$@"
