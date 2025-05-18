aa0bf19ccbacf34c2379826fdfa6d75c452b2bf4
Hannan <abdulhannan0107@gmail.com>
2025-05-18 13:49:00 +0530

    Update padel.sh 2.0

--- padel.sh
+++ padel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Set JAVA_HOME explicitly
+# Set JAVA_HOME
 JAVA_HOME=/opt/java/openjdk
-# Run Java with reduced memory settings
+# Run Java with the correct path
 $JAVA_HOME/bin/java -Xms128m -Xmx256m -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar "$@"
